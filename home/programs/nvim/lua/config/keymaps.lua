80fa6649a0721619378686f9c4a7ddd80bf59f67
psyclyx <me@psyclyx.xyz>
2024-09-01 23:35:30 -0700

    add :noh binding

--- home/programs/nvim/lua/config/keymaps.lua
+++ home/programs/nvim/lua/config/keymaps.lua
@@ -5,3 +5,4 @@ vim.g.maplocalleader = ","
 vim.keymap.set("n", "<leader>bD", "<cmd>%bd!|e#|bd#<cr>|'\"", { silent = true, desc = "kill other buffers" })
 vim.keymap.set("n", "<leader>bd", "<cmd>bd!<cr>", { silent = true, desc = "kill current buffer" })
 vim.keymap.set("n", "<leader>fe", "<cmd>:Explore<cr>", { silent = true, desc = "explore" })
+vim.keymap.set("n", "<leader>n", "<cmd>:nohlsearch<cr>", { silent = true, desc = "no highlight" })

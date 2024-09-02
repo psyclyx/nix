87ef8a71656f5950c16c41d27c4cd522c366089b
psyclyx <me@psyclyx.xyz>
2024-09-01 23:35:29 -0700

    add stylua package

--- programs/nvim/default.nix
+++ programs/nvim/default.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 
 {
-  home.packages = [ pkgs.neovim ];
+  home.packages = [ pkgs.neovim pkgs.stylua ];
   home.file.".config/nvim/init.lua".source = ./init.lua;
   home.file.".config/nvim/coc-settings.json".source = ./coc-settings.json;
   home.file.".config/nvim/lua".source = ./lua;

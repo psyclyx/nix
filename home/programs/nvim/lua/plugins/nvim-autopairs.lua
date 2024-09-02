5e3f0c7016a8f2edcb50d6aa79d52de88a02eec1
psyclyx <me@psyclyx.xyz>
2024-09-01 23:35:30 -0700

    fix typo in nvim-autopairs config

--- home/programs/nvim/lua/plugins/nvim-autopairs.lua
+++ home/programs/nvim/lua/plugins/nvim-autopairs.lua
@@ -2,7 +2,7 @@ return {
 	"windwp/nvim-autopairs",
 	dependencies = { "nvim-treesitter/nvim-treesitter" },
 	opts = {
-		disabled_filetype = { "TelescopePrompt", "clojure" },
+		disable_filetype = { "TelescopePrompt", "clojure" },
 		check_ts = true,
 		map_bs = false,
 		map_cr = false,

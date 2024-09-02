1a00c32711c0f306b0e15f271013df7189598a47
psyclyx <me@psyclyx.xyz>
2024-09-01 23:35:30 -0700

    lazydev to own plugin file

---
+++ config/nvim/lua/plugins/lazydev.lua
@@ -0,0 +1,8 @@
+return {
+	"folke/lazydev.nvim",
+	dependencies = { "Bilal2453/luvit-meta" },
+	ft = "lua",
+	opts = { library = {
+		{ path = "luvit-meta/library", words = { "vim%.uv" } },
+	} },
+}
--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -1,11 +1,3 @@
 return {
 	"neovim/nvim-lspconfig",
-	{
-		"folke/lazydev.nvim",
-		dependencies = { "Bilal2453/luvit-meta" },
-		ft = "lua",
-		opts = { library = {
-			{ path = "luvit-meta/library", words = { "vim%.uv" } },
-		} },
-	},
 }

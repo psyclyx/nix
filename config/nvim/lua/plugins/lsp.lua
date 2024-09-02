883526ee229f938667f559f3c4b5163f61644272
psyclyx <me@psyclyx.xyz>
2024-09-01 23:35:30 -0700

    add lsp plugin

---
+++ config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,13 @@
+return {
+	"neovim/nvim-lspconfig",
+	"hrsh7th/nvim-cmp",
+	"onsails/lspkind.nvim",
+	"hrsh7th/cmp-nvim-lsp",
+	"hrsh7th/cmp-path",
+	"hrsh7th/cmp-nvim-lsp-signature-help",
+	"hrsh7th/cmp-nvim-lsp-document-symbol",
+	"hrsh7th/cmp-buffer",
+	"hrsh7th/cmp-cmdline",
+	"JMarkin/cmp-diag-codes",
+	"saadparwaiz1/cmp_luasnip",
+}

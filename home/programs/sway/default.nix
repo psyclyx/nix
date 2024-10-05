ce324d7a7e006946bdaf99c9af5f4f0f1bda2233
psyclyx <me@psyclyx.xyz>
2024-10-04 22:06:41 -0700

    wofi, waybar, brightness, volume

--- home/programs/sway/default.nix
+++ home/programs/sway/default.nix
@@ -6,15 +6,28 @@
     mako
   ];
   programs.swaylock.enable = true;
+
   wayland.windowManager.sway = {
     enable = true;
     config = {
       modifier = "Mod4";
       terminal = "wezterm";
-      menu = "${pkgs.rofi-wayland}/bin/rofi -show drun -modi drun";
+      menu = "${pkgs.wofi}/bin/wofi --show run";
+      bars = [
+        {command = "${pkgs.waybar}/bin/waybar";}
+      ];
     };
     extraConfig = ''
       output * scale 1
+
+      # Brightness
+      bindsym XF86MonBrightnessDown exec light -U 10
+      bindsym XF86MonBrightnessUp exec light -A 10
+
+      # Volume
+      bindsym XF86AudioRaiseVolume exec 'pactl set-sink-volume @DEFAULT_SINK@ +1%'
+      bindsym XF86AudioLowerVolume exec 'pactl set-sink-volume @DEFAULT_SINK@ -1%'
+      bindsym XF86AudioMute exec 'pactl set-sink-mute @DEFAULT_SINK@ toggle'
     '';
   };
 }

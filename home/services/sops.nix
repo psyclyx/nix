bade3315032811edcbb6846016da00086c7d250e
psyclyx <me@psyclyx.xyz>
2024-10-27 01:14:23 -0700

    symlink ssh keys in sops

--- home/services/sops.nix
+++ home/services/sops.nix
@@ -1,5 +1,16 @@
 {config, ...}: {
   sops = {
     age.keyFile = config.xdg.configFile."sops/age/keys.txt";
+    defaultSopsFile = ../../.sops.yaml;
+    secrets = let
+      home = config.home.homeDirectory;
+    in {
+      "ssh/psyclyx.json" = {
+        path = "${home}/.ssh/id_psyclyx";
+      };
+      "ssh/alice157.json" = {
+        path = "${home}/.ssh/id_alice157";
+      };
+    };
   };
 }

d4eadc052aa8437a43959b5ef0fbb494ee25710b
psyclyx <me@psyclyx.xyz>
2024-11-20 17:59:44 -0800

    fix ix networking

--- hosts/ix/network.nix
+++ hosts/ix/network.nix
@@ -1,5 +1,7 @@
 {...}: {
-  system.network = {
+  networking.firewall.allowedTCPPorts = [22];
+
+  systemd.network = {
     enable = true;
     networks."30-wan" = {
       matchConfig.Name = "eth0";
@@ -8,15 +10,15 @@
         "162.55.34.172"
         "2a01:4f8:c17:90e8::1"
       ];
+      routes = [
+        {
+          routeConfig = {
+            Gateway = "172.31.1.1";
+            GatewayOnLink = true;
+          };
+        }
+        {routeConfig.Gateway = "fe80::1";}
+      ];
     };
-    routes = [
-      {
-        routeConfig = {
-          Gateway = "172.31.1.1";
-          GatewayOnLink = true;
-        };
-      }
-      {routeConfig.Gateway = "fe80::1";}
-    ];
   };
 }

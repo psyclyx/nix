e352fe551dc3acb7fd5a08d8957dd1ed67df2246
psyclyx <me@psyclyx.xyz>
2024-11-20 17:58:07 -0800

    configure openssh

--- modules/nixos/services/openssh.nix
+++ modules/nixos/services/openssh.nix
@@ -1,3 +1,10 @@
 {...}: {
-  services.openssh.enable = true;
+  services.openssh = {
+    enable = true;
+    settings = {
+      PermitRootLogin = "no";
+      PasswordAuthentication = false;
+      KbdInteractiveAuthentication = false;
+    };
+  };
 }

0158f4ed415a94fad6addc8823be967e93b504f7
psyclyx <me@psyclyx.xyz>
2024-11-20 18:01:01 -0800

    fix ix users

--- hosts/ix/users.nix
+++ hosts/ix/users.nix
@@ -1,12 +1,14 @@
 {pkgs, ...}: {
-  user = {
+  users = {
     users = {
-      name = "psyc";
-      home = "/home/psyc";
-      shell = pkgs.zsh;
-      isNormalUser = true;
-      extraGroups = ["wheel"];
-      openssh.authorizedKeys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEwUKqMso49edYpzalH/BFfNlwmLDmcUaT00USWiMoFO me@psyclyx.xyz"];
+      psyc = {
+        name = "psyc";
+        home = "/home/psyc";
+        shell = pkgs.zsh;
+        isNormalUser = true;
+        extraGroups = ["wheel"];
+        openssh.authorizedKeys.keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEwUKqMso49edYpzalH/BFfNlwmLDmcUaT00USWiMoFO me@psyclyx.xyz"];
+      };
     };
   };
   home-manager = {

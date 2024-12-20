df5b48986fb091ccd3801ddf1a13df3de3c83601
psyclyx <me@psyclyx.xyz>
2024-12-20 05:28:42 -0800

    add tailscale

--- hosts/halo/default.nix
+++ hosts/halo/default.nix
@@ -9,6 +9,7 @@
     ../../modules/darwin/system/home-manager.nix
     ../../modules/darwin/system/settings.nix
     ../../modules/darwin/services/nix-daemon.nix
+    ../../modules/darwin/services/tailscale.nix
     ../../modules/darwin/programs/raycast.nix
     ../../modules/nixos/programs/zsh.nix
   ];

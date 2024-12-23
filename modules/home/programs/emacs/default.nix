4312e8585e472d9c9d4b2cdff4e7c92c97706906
psyclyx <me@psyclyx.xyz>
2024-12-22 16:28:41 -0800

    switch back to old elisp-based config

--- modules/home/programs/emacs/default.nix
+++ modules/home/programs/emacs/default.nix
@@ -1,37 +1,18 @@
 {
   config,
   pkgs,
-  inputs,
   ...
 }: let
   packageConfig = import ./packages.nix;
-  patch = path: "${inputs.homebrew-emacs-plus}/patches/emacs-29/${path}";
-  baseEmacs =
-    if pkgs.stdenv.isDarwin
-    then
-      pkgs.emacsGit.overrideAttrs (old: {
-        patches =
-          (old.patches or [])
-          ++ [
-            (patch "fix-window-role.patch")
-            (patch "no-frame-refocus-cocoa.patch")
-            (patch "round-undecorated-frame.patch")
-            (patch "system-appearance.patch")
-          ];
-        configureFlags =
-          (old.configureFlags or [])
-          ++ [
-            "LDFLAGS=-headerpad_max_install_names"
-          ];
-      })
-    else pkgs.emacsPgtk;
-  emacs = pkgs.emacsWithPackagesFromUsePackage {
-    config = ./config.org;
-    defaultInitFile = true;
-    package = pkgs.emacs-git;
-    alwaysTangle = true;
-    alwaysEnsure = true;
-  };
 in {
-  home.packages = [emacs];
+  programs.emacs = {
+    enable = true;
+    package = pkgs.emacs-git;
+    extraPackages = epkgs:
+      (packageConfig.emacsPackages epkgs) ++ (packageConfig.systemPackages pkgs);
+  };
+  home.file."${config.xdg.configHome}/emacs" = {
+    source = ./emacs;
+    recursive = true;
+  };
 }

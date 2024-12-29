20570be6b9a7552ef9cfb98f78462229ed27596c
psyclyx <me@psyclyx.xyz>
2024-12-28 19:42:27 -0800

    put emacsclient on PATH of Emacs.app

--- modules/home/programs/emacs/default.nix
+++ modules/home/programs/emacs/default.nix
@@ -9,15 +9,24 @@
   packages = packageConfig.systemPackages pkgs;
   relPath = lib.strings.removePrefix (toString inputs.self) (toString ./config);
   configPath = "${config.home.homeDirectory}/projects/nix" + relPath;
+  emacs = if pkgs.stdenv.isDarwin
+            then pkgs.emacs-30
+            else pkgs.emacs-git;
+  emacsclient = pkgs.stdenv.mkDerivation {
+    pname = "emacsclient";
+    version = emacs.version;
+    phases = [ "installPhase" ];
+    installPhase = ''
+      mkdir -p $out/bin
+	  cp ${emacs}/bin/emacsclient $out/bin/
+    '';
+  };
 in {
   programs.emacs = {
     enable = true;
-    package =
-      if pkgs.stdenv.isDarwin
-      then pkgs.emacs-30
-      else pkgs.emacs-git;
+    package = emacs;
     extraPackages = epkgs:
-      (packageConfig.emacsPackages epkgs) ++ packages;
+      (packageConfig.emacsPackages epkgs) ++ packages ++ [emacsclient];
   };
   home.file.".config/emacs".source = config.lib.file.mkOutOfStoreSymlink configPath;
 }

0746697ba213edf6942ad983b5c212a1e8ea86cd
psyclyx <me@psyclyx.xyz>
2024-12-30 18:31:57 -0800

    sigil: more nvidia gpu

--- hosts/sigil/hardware.nix
+++ hosts/sigil/hardware.nix
@@ -1,9 +1,20 @@
-{inputs, ...}: {
+{
+  inputs,
+  config,
+  ...
+}: {
   hardware.cpu.amd.updateMicrocode = true;
   hardware.enableRedistributableFirmware = true;
   imports = [
     inputs.nixos-hardware.nixosModules.common-cpu-amd
-    inputs.nixos-hardware.nixosModules.common-gpu-nvidia
   ];
   hardware.nvidia.open = true;
+
+  services.xserver.videoDrivers = ["nvidia"];
+  hardware.graphics.enable = true;
+
+  powerManagement.enable = false;
+  powerManagement.finegrained = true;
+  nvidiaSettings = true;
+  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
 }

60dc87c7ad0ea20f1c9a7ec303203281ebcb77e4
psyclyx <me@psyclyx.xyz>
2024-12-30 18:16:36 -0800

    use cachix on nixos

--- modules/nixos/system/nix.nix
+++ modules/nixos/system/nix.nix
@@ -4,9 +4,11 @@
       trusted-users = ["@builders"];
       substituters = [
         "https://nix-community.cachix.org"
+        "https://psyclyx.cachix.org"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "psyclyx.cachix.org-1:UFwKXEDn3gLxIW9CeXGdFFUzCIjj8m6IdAQ7GA4XfCk="
       ];
     };
     gc = {

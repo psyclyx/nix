0416880c4d0ce809d244acbee217706e591e6ce6
psyclyx <me@psyclyx.xyz>
2024-12-30 18:24:07 -0800

    fix amd modules

--- hosts/sigil/hardware.nix
+++ hosts/sigil/hardware.nix
@@ -2,7 +2,7 @@
   hardware.cpu.amd.updateMicrocode = true;
   hardware.enableRedistributableFirmware = true;
   imports = [
-    inputs.nixos-hardware.nixosModules.common.cpu.amd
-    inputs.nixos-hardware.nixosModules.common.gpu.nvidia.ampere
+    inputs.nixos-hardware.nixosModules.common-cpu-amd
+    inputs.nixos-hardware.nixosModules.common-gpu-nvidia
   ];
 }

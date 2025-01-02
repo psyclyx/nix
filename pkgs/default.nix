09623eacdd529b9546a2c730f0c7dc1e5518b940
psyclyx <me@psyclyx.xyz>
2025-01-02 06:00:02 -0800

    pkgs: add love-darwin-bin

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,4 +1,5 @@
 final: prev: {
   cljstyle = final.callPackage ./cljstyle {};
   pharo12-stable = final.callPackage ./pharo12-stable.nix {};
+  love-darwin-bin = final.callPackage ./love-darwin-bin.nix {};
 }

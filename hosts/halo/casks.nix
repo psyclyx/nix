3535f379a7882c2338f105e5533555313acd53a4
psyclyx <me@psyclyx.xyz>
2025-01-11 02:55:43 -0800

    halo: homebrew continues to befuddle me. idek at this point

--- hosts/halo/casks.nix
+++ hosts/halo/casks.nix
@@ -1,4 +1,11 @@
-{...}: {
+{inputs, ...}: {
+  nix-homebrew = {
+    taps = with inputs; {
+      "homebrew/bundle" = homebrew-bundle;
+      "homebrew/core" = homebrew-core;
+      "homebrew/cask" = homebrew-cask;
+    };
+  };
   homebrew.casks = [
     "firefox"
     "orcaslicer"

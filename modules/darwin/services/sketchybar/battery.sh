095d0010358e34c6be363ec7601bd0647d2072f1
psyclyx <me@psyclyx.xyz>
2025-01-11 03:00:27 -0800

    sketchybar: clean up sb battery

--- modules/darwin/services/sketchybar/battery.sh
+++ modules/darwin/services/sketchybar/battery.sh
@@ -1,41 +1,37 @@
 #!/usr/bin/env bash
-
-exec &>> /tmp/sk-bat.log
-set -x
-
-echo "bat start" > /tmp/sk-bat.log
-
-{ which grep; which pmset; which cut; } >> /tmp/sk-bat.log
-
-pmset -g batt
 PERCENT=$(pmset -g batt | grep -Eo '[[:digit:]]+%' | cut -d% -f1)
-CHARGING=$(pmset -g batt | grep 'AC Power')
+CHARGING=$(pmset -g batt | grep 'AC Power' || echo "")
 HIGHLIGHT=${CHARGING:+on}
 HIGHLIGHT=${HIGHLIGHT:-off}
 
-printenv >> /tmp/sk-bat.log
-
 get_icon() {
-    local percent=$1
-    local charging=$2
-    local base_icon=${charging:+"󰂅󰂋󰂊󰢞󰂉󰢝󰂈󰂇󰂆󰢜󰢟":"󰁹󰂂󰂁󰂀󰁿󰁾󰁽󰁼󰁻󰁺󰂎"}
+  local percent=$1
+  local charging=$2
+  local charging_icons=("󰢟" "󰢜" "󰂆" "󰂇" "󰂈" "󰢝" "󰂉" "󰢞" "󰂊" "󰂋" "󰂅")
+  local battery_icons=("󰂎" "󰁺" "󰁻" "󰁼" "󰁽" "󰁾" "󰁿" "󰂀" "󰂁" "󰂂" "󰁹")
 
-    case $percent in
-        100) echo "${base_icon:0:4}" ;;
-        9[0-9]) echo "${base_icon:4:4}" ;;
-        8[0-9]) echo "${base_icon:8:4}" ;;
-        7[0-9]) echo "${base_icon:12:4}" ;;
-        6[0-9]) echo "${base_icon:16:4}" ;;
-        5[0-9]) echo "${base_icon:20:4}" ;;
-        4[0-9]) echo "${base_icon:24:4}" ;;
-        3[0-9]) echo "${base_icon:28:4}" ;;
-        2[0-9]) echo "${base_icon:32:4}" ;;
-        1[0-9]) echo "${base_icon:36:4}" ;;
-        *) echo "${base_icon:40:4}"
-    esac
+  local icons
+  if [ -n "$charging" ]; then
+    icons=("${charging_icons[@]}")
+  else
+    icons=("${battery_icons[@]}")
+  fi
+
+  case $percent in
+    100) echo "${icons[10]}" ;;
+    9[0-9]) echo "${icons[9]}" ;;
+    8[0-9]) echo "${icons[8]}" ;;
+    7[0-9]) echo "${icons[7]}" ;;
+    6[0-9]) echo "${icons[6]}" ;;
+    5[0-9]) echo "${icons[5]}" ;;
+    4[0-9]) echo "${icons[4]}" ;;
+    3[0-9]) echo "${icons[3]}" ;;
+    2[0-9]) echo "${icons[2]}" ;;
+    1[0-9]) echo "${icons[1]}" ;;
+    [0-9]) echo "${icons[0]}" ;;
+  esac
 }
 
-echo "getting icon" >> /tmp/sk-bat.log
 ICON=$(get_icon "${PERCENT}" "${CHARGING}")
 
 battery=(
@@ -43,8 +39,5 @@ battery=(
   "label=${PERCENT}%"
   "icon.highlight=${HIGHLIGHT}"
 )
-
-echo "setting" >> /tmp/sk-bat.log
-printenv >> /tmp/sb-bat.log
 
 sketchybar --set "${NAME}" "${battery[@]}"

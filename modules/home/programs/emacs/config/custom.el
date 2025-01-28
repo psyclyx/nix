f4aedb510d191a1b510e81ff988fd603e42b8d2a
psyclyx <me@psyclyx.xyz>
2025-01-27 20:40:27 -0800

    emacs: lispyville bindings, doom-modeline settings, config.org formatting

--- modules/home/programs/emacs/config/custom.el
+++ modules/home/programs/emacs/config/custom.el
@@ -4,7 +4,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages nil))
+ '(safe-local-variable-values
+   '((eval define-clojure-indent (:require 0) (:import '(0 (0)))
+           (defrecord '(1 nil (:defn))) (forv 1) (for+ 1)
+           (future-with 1) (start-unless 1) (stop-when 1) (do-at 1)
+           (thrown? 1) (thrown-with-msg? 2)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

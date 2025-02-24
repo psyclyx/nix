5e2560385cc3dd264efbe6a728dde067ccf1058b
psyclyx <me@psyclyx.xyz>
2025-02-24 09:20:41 -0800

    git: enable fsmonitor

--- modules/home/base/programs/git.nix
+++ modules/home/base/programs/git.nix
@@ -7,6 +7,11 @@
     enable = lib.mkDefault true;
     userName = lib.mkDefault config.my.user.name;
     userEmail = lib.mkDefault config.my.user.email;
-    iniContent = lib.mkMerge [{"pull" = {"rebase" = true;};}];
+    iniContent = lib.mkMerge [
+      {
+        "pull" = {"rebase" = true;};
+        "core" = {"fsmonitor" = true;};
+      }
+    ];
   };
 }

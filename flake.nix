cb01e1dccb20706ee34b65ad9ed5ea5badc64490
psyc <me@psyclyx.xyz>
2025-03-09 03:00:16 -0700

    flake: clean up inputs and justify each one

--- flake.nix
+++ flake.nix
@@ -2,79 +2,105 @@
   description = "nixos/nix-darwin configurations";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    # ==== Core ====
 
+    # Main package repository
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nixpkgs-master.url = "github:NixOS/nixpkgs/master";
 
+    # Simple NixOS modules for common hardware configurations
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
+    # NixOS-esque configuration for Darwin (MacOS)
     nix-darwin = {
       url = "github:LnL7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    # NixOS-esque configuration of home directories
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    sops-nix = {
-      url = "github:Mic92/sops-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # ==== Deploy ====
 
+    # Declarative disk partitioning that doubles as NixOS config
     disko = {
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    # Secrets decrypted at runtime, for NixOS/nix-darwin and home-manager
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # ==== Programs ====
+
+    # `nix-darwin` module to manage a homebrew installation
+    # (`nix-darwin` can manage packages/casks from homebrew OOTB,
+    #  but doesn't have support for installing homebrew itself)
     nix-homebrew = {
       url = "github:zhaofengli-wip/nix-homebrew";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nix-darwin.follows = "nix-darwin";
     };
 
-    nix-darwin-emacs = {
-      url = "github:nix-giant/nix-darwin-emacs";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
+    # Track newer versions of `emacs` (NixOS) and its package repositories
     emacs-overlay = {
       url = "github:nix-community/emacs-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    # Track newer versions of `emacs` on Darwin
+    nix-darwin-emacs = {
+      url = "github:nix-giant/nix-darwin-emacs";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # ==== Non-flakes ====
+
+    # `zsh` prompt
+    powerlevel10k = {
+      url = "github:romkatv/powerlevel10k";
+      flake = false;
+    };
+
+    # ---- Homebrew taps ----
+
+    # Base taps
     homebrew-core = {
       url = "github:homebrew/homebrew-core";
       flake = false;
     };
-
     homebrew-bundle = {
       url = "github:homebrew/homebrew-bundle";
       flake = false;
     };
-
     homebrew-cask = {
       url = "github:homebrew/homebrew-cask";
       flake = false;
     };
 
+    # Provides `vault` on darwin, as building it takes forever.
     homebrew-hashicorp = {
       url = "github:hashicorp/homebrew-tap";
       flake = false;
     };
 
+    # VPN client
     homebrew-conductorone = {
       url = "github:conductorone/cone";
       flake = false;
     };
 
+    # Includes `jet`, like `jq` for EDN
+    # TODO: write/upstream a derivation to do this through nix,
+    # no fundamental reason this needs to come through homebrew
     homebrew-borkdude = {
       url = "github:borkdude/homebrew-brew";
-      flake = false;
-    };
-
-    powerlevel10k = {
-      url = "github:romkatv/powerlevel10k";
       flake = false;
     };
   };

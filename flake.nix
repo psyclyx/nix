5a353c67ff90b9fafe8cd8e9938efcab4b678bd3
psyc <me@psyclyx.xyz>
2025-03-09 11:11:34 -0700

    emacs: use correct package in cache output

--- flake.nix
+++ flake.nix
@@ -200,7 +200,7 @@
     # For cache
     packages = {
       "aarch64-darwin".emacs = (pkgsFor "aarch64-darwin").emacs-30;
-      "x86_64-linux".emacs = (pkgsFor "x86_64-linux").emacs30-pgtk;
+      "x86_64-linux".emacs = (pkgsFor "x86_64-linux").emacs-unstable-pgtk;
     };
   };
 }

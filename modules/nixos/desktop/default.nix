4687b13eaf7be5f42925c87bd51951936edf1ccc
psyc <me@psyclyx.xyz>
2025-03-09 21:12:39 -0700

    desktop: use greetd module

--- modules/nixos/desktop/default.nix
+++ modules/nixos/desktop/default.nix
@@ -6,18 +6,34 @@
 }: let
   desktopCfg = config.psyclyx.desktop;
 in {
-  imports = [./sway.nix];
+  imports = [./sway.nix ./tuigreet.nix];
   options.psyclyx.desktop = {
     enable = lib.mkEnableOption ''
       Set up a graphical environment.
     '';
   };
 
-  config =
-    lib.mkIf desktopCfg.enable {
-      services = {
-        gvfs.enable = lib.mkDefault true;
-        gnome.gnome-keyring = lib.mkDefault true;
+  config = lib.mkIf desktopCfg.enable {
+    psyclyx.desktop.sway.enable = lib.mkDefault true;
+    services = {
+      gvfs.enable = lib.mkDefault true;
+      gnome.gnome-keyring = lib.mkDefault true;
+
+      tuigreet = {
+        time = true;
+        user-menu = true;
+        remember = true;
+        asterisks = true;
+        cmd = "${pkgs.sway}/bin/sway";
+      };
+
+      greetd = {
+        enable = lib.mkDefault true;
+        vt = lib.mkDefault 2;
+        settings = {
+          default_session.command = config.services.greetd.tuigreet.command;
+        };
       };
     };
+  };
 }

e50faa1847f0d71106758bc88c6a411a90ec987e
psyclyx <me@psyclyx.xyz>
2025-10-14 09:04:05 -0700

    breaking, nixos: remove dns option
    currently not needed:
    only host that uses this is sigil, and we want to get dns from dhcp.
    other hosts will eventually want this too, but it'll require a bit
    more thought.

--- modules/nixos/network/default.nix
+++ modules/nixos/network/default.nix
@@ -15,15 +15,6 @@ let
           default = [ name ];
         };
         enableDHCP = lib.mkEnableOption "DHCP";
-        dns = lib.mkOption {
-          type = lib.types.listOf lib.types.str;
-          default = [
-            "2606:4700:4700::1111"
-            "2606:4700:4700::1001"
-            "1.1.1.1"
-            "1.0.0.1"
-          ];
-        };
         requiredForOnline = lib.mkEnableOption "participation in wait-online.target";
       };
     };
